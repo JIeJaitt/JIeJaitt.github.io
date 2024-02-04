e6ddacbb9d03d771d718479c763da5056b754877
JIeJaitt <498938874@qq.com>
2024-02-04 14:46:00 +0800

    Site updated: 1707029160

---
+++ git_commit_push_timestamp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	// 获取当前时间戳
+	timestamp := time.Now().Unix()
+
+	// 构造commit消息
+	commitMessage := fmt.Sprintf("Site updated: %d", timestamp)
+
+	// 执行git add命令
+	cmdAdd := exec.Command("git", "add", "-A")
+	cmdAdd.Stdout = os.Stdout
+	cmdAdd.Stderr = os.Stderr
+	if err := cmdAdd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	// 执行git commit命令
+	cmdCommit := exec.Command("git", "commit", "-m", commitMessage)
+	cmdCommit.Stdout = os.Stdout
+	cmdCommit.Stderr = os.Stderr
+	if err := cmdCommit.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	// 执行git push命令
+	cmdPush := exec.Command("git", "push")
+	cmdPush.Stdout = os.Stdout
+	cmdPush.Stderr = os.Stderr
+	if err := cmdPush.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("代码已成功推送到远程仓库！")
+}
