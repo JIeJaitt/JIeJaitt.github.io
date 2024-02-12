128218c14170f146171c2bd70e4f71620024caf2
JIeJaitt <498938874@qq.com>
2024-02-12 22:31:08 +0800

    Site updated: 2024-02-12 22:31:08

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,10 +1,3 @@
-// const css = hexo.extend.helper.get('css').bind(hexo);
-// const js = hexo.extend.helper.get('js').bind(hexo);
-
-// hexo.extend.injector.register('head_end', () => {
-// 	return css('/css/navbar.css');
-//   });
-
 hexo.extend.injector.register('head_end', () => `
    <style>
       /* 置顶导航栏样式 */
