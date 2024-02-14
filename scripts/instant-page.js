52cd43613f090d314a129c2608c9187b7e7763fd
JIeJaitt <498938874@qq.com>
2024-02-14 12:55:52 +0800

    Site updated: 2024-02-14 12:55:52

---
+++ scripts/instant-page.js
@@ -0,0 +1,1 @@
+hexo.extend.injector.register('body_end', '<script src="//instant.page/5.2.0" type="module" integrity="sha384-jnZyxPjiipYXnSU0ygqeac2q7CVYMbh84q0uHVRRxEtvFPiQYbXWUorga2aqZJ0z"></script>', 'default');
