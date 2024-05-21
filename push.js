98e41f15b5f2da6258234075f5b0086c257186d1
JIeJaitt <498938874@qq.com>
2024-05-21 23:07:45 +0800

    Site updated: 2024-05-21 15:07:45

--- push.js
+++ push.js
@@ -4,34 +4,39 @@ const { exec } = require('child_process');
 const currentTime = new Date();
 
 // 格式化时间戳
-const timestamp = currentTime.toISOString().replace('T', ' ').substring(0, 19);
+const timestamp = currentTime.toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
 // 构造commit消息
 const commitMessage = `Site updated: ${timestamp}`;
 
-// 执行git命令的函数
-function executeCommand(command, args) {
-  return new Promise((resolve, reject) => {
-    const cmd = exec(`${command} ${args.join(' ')}`, (error, stdout, stderr) => {
-      if (error) {
-        reject(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        reject(`stderr: ${stderr}`);
-        return;
-      }
-      resolve(stdout);
-    });
-
-    cmd.stdout.pipe(process.stdout);
-    cmd.stderr.pipe(process.stderr);
-  });
-}
-
 // 执行git add命令
-executeCommand('git', ['add', '-A'])
-  .then(() => executeCommand('git', ['commit', '-m', `"${commitMessage}"`]))
-  .then(() => executeCommand('git', ['push']))
-  .then(() => console.log('代码已成功推送到远程仓库！'))
-  .catch(err => console.error('Failed:', err));
+exec('git add -A', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`执行git add命令时出错: ${err}`);
+    return;
+  }
+  console.log(stdout);
+  console.error(stderr);
+
+  // 执行git commit命令
+  exec(`git commit -m "${commitMessage}"`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`执行git commit命令时出错: ${err}`);
+      return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+
+    // 执行git push命令
+    exec('git push', (err, stdout, stderr) => {
+      if (err) {
+        console.error(`执行git push命令时出错: ${err}`);
+        return;
+      }
+      console.log(stdout);
+      console.error(stderr);
+
+      console.log('代码已成功推送到远程仓库！');
+    });
+  });
+});
