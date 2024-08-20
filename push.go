c432d4daa6096e701c643f37934a72fec5e05a1b
JIeJaitt <498938874@qq.com>
2024-08-20 20:53:25 +0800

    Site updated: 2024-08-20 20:52:57

--- push.go
+++ push.go
@@ -9,6 +9,14 @@ import (
 	"C"
 )
 
+func runCommand(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fmt.Printf("正在执行: %s %s\n", command, args)
+	return cmd.Run()
+}
+
 func main() {
 	// 获取当前时间
 	currentTime := time.Now()
@@ -19,28 +27,29 @@ func main() {
 	// 构造commit消息
 	commitMessage := fmt.Sprintf("Site updated: %s", timestamp)
 
+	// 执行hexo g命令
+	if err := runCommand("hexo", "g"); err != nil {
+		log.Fatal("执行 hexo g 命令失败:", err)
+	}
+
+	// 执行git pull命令
+	if err := runCommand("git", "pull"); err != nil {
+		log.Fatal("执行 git pull 命令失败:", err)
+	}
+
 	// 执行git add命令
-	cmdAdd := exec.Command("git", "add", "-A")
-	cmdAdd.Stdout = os.Stdout
-	cmdAdd.Stderr = os.Stderr
-	if err := cmdAdd.Run(); err != nil {
-		log.Fatal(err)
+	if err := runCommand("git", "add", "-A"); err != nil {
+		log.Fatal("执行 git add 命令失败:", err)
 	}
 
 	// 执行git commit命令
-	cmdCommit := exec.Command("git", "commit", "-m", commitMessage)
-	cmdCommit.Stdout = os.Stdout
-	cmdCommit.Stderr = os.Stderr
-	if err := cmdCommit.Run(); err != nil {
-		log.Fatal(err)
+	if err := runCommand("git", "commit", "-m", commitMessage); err != nil {
+		log.Fatal("执行 git commit 命令失败:", err)
 	}
 
 	// 执行git push命令
-	cmdPush := exec.Command("git", "push")
-	cmdPush.Stdout = os.Stdout
-	cmdPush.Stderr = os.Stderr
-	if err := cmdPush.Run(); err != nil {
-		log.Fatal(err)
+	if err := runCommand("git", "push"); err != nil {
+		log.Fatal("执行 git push 命令失败:", err)
 	}
 
 	fmt.Println("代码已成功推送到远程仓库！")
